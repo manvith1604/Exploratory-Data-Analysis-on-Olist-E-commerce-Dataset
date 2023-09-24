a351a633a540b89eaea27b9b8603224a60523819
Manvith B Y <66794160+manvith1604@users.noreply.github.com>
2023-09-25 00:20:04 +0530

    Update Query.sql

--- SQL Queries/Query.sql
+++ SQL Queries/Query.sql
@@ -1,1 +1,17 @@
+1.	Top 10 highest sold products 
+  
+SELECT 
+    i.product_id,
+    p.name,
+    p.category,
+    COUNT(i.order_id) AS 'Order_count'
+FROM
+    items i,
+    products p
+WHERE
+    i.product_id = p.product_id
+GROUP BY i.product_id , p.name , p.category
+ORDER BY Order_count DESC
+LIMIT 10;
+
 
